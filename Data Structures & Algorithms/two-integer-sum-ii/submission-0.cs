f705cfba9ea0abc0334902586fde85cbd2594136
sims438 <39458161+sims438@users.noreply.github.com>
2026-07-05 14:29:31 +0530

    Add: two-integer-sum-ii - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-0.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public int[] TwoSum(int[] numbers, int target) {
+        int i=0;
+        int j=numbers.Length-1;
+        while(i<j)
+        {
+            if(numbers[i]+numbers[j] == target)
+            {
+               return new int[] { i + 1, j + 1 };
+            }
+            else if(numbers[i]+numbers[j] > target)
+            {   
+                j--;
+            }
+            else
+            {
+                i++;
+            }
+        }
+        return new int[] { -1, -1 };
+        
+    }
+}
